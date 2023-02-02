04a4d72b0edddc1a5ffb44ece95bc0e143340c2a
Marta Santos <mdpesa@gmail.com>
2023-02-02 23:16:36 +0100

    Time Converter correction

--- src/components/TimeConverter/TimeConverter.js
+++ src/components/TimeConverter/TimeConverter.js
@@ -2,16 +2,23 @@ import styles from './TimeConverter.module.scss';
 
 const TimeConverter = (props) => {
   const convertMs = (miliseconds) => {
-    let milliseconds = Math.floor(miliseconds % 1000);
+    let ms = Math.floor(miliseconds % 1000);
     let seconds = Math.floor((miliseconds / 1000) % 60);
     let minutes = Math.floor((miliseconds / (1000 * 60)) % 60);
     let hours = Math.floor((miliseconds / (1000 * 60 * 60)) % 24);
 
-    hours = hours < 10 ? '0' + hours : hours;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+    //hours = hours < 10 ? '0' + hours : hours;
+    //minutes = minutes < 10 ? '0' + minutes : minutes;
+    //seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    return hours + ':' + minutes + ':' + seconds + '.' + milliseconds;
+    hours = String(hours).padStart(2, '0');
+    minutes = String(minutes).padStart(2, '0');
+    seconds = String(seconds).padStart(2, '0');
+    ms = String(miliseconds).padStart(3, '0')
+
+    return `${hours}:${minutes}:${seconds}.${ms}`;
+    // return hours + ':' + minutes + ':' + seconds + '.' + ms;
+    //return `${hours.padStart(2,'0')}:${minutes.padStart(2,'0')}:${seconds.padStart(2,'0')}.${ms.padStart(3,'0')}`;
   };
 
   return <div className={styles.timeConverter}>{convertMs(props.time)}</div>;
